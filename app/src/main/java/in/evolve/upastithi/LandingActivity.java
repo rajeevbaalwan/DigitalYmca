e2fc0828c35abcbed13f00d6166a26454b69f38a
rajeevyadav <rajeevy899@gmail.com>
2016-10-26 20:59:08 +0530

    Project Started

---
+++ app/src/main/java/in/evolve/upastithi/LandingActivity.java
@@ -0,0 +1,17 @@
+package in.evolve.upastithi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.evolve.upastithi.Utilities.Utils;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+
+    }
+}
